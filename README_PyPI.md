9ae330851e4a3a70570199fc7a66a4674c952afe
aloytag <ariel.loyarte@gmail.com>
2024-01-23 13:49:34 -0300

    Firs commit for version 1.0

---
+++ README_PyPI.md
@@ -0,0 +1,49 @@
+# mpl-modernqt
+A modern looking backend for Matplotlib based on the official Qt backend. Its main features are:
+
+* `Phosphor Icons` (monochromatic) thanks to the [QtAwesome](https://github.com/spyder-ide/qtawesome) package.
+* Modern theme with the [PyQtDarkTheme](https://github.com/5yutan5/PyQtDarkTheme) package.
+* `Qt5` and `Qt6` support.
+
+
+## Installation
+Using ```pip```, the official Python package manager...
+
+On GNU/Linux and MacOS execute the following order in a terminal:
+```bash
+pip install matplotlib-modernqt -U
+```
+
+On MS Windows you may prefer:
+```bash
+python -m pip matplotlib-modernqt -U
+```
+
+## Usage
+
+```python
+from matplotlib import use
+
+use('module://mpl_modernqt.backend')
+```
+
+## Example code
+
+```python
+import numpy as np
+from matplotlib import use
+import matplotlib.pyplot as plt
+
+use('module://mpl_modernqt.backend')
+
+x = np.linspace(0, 7, 300)
+y = np.sin(x)
+
+plt.plot(x, y)
+plt.show()
+```
+
+
+## License
+
+This project uses the [MIT license](https://github.com/aloytag/mpl-modernqt/blob/main/LICENSE).
---
+++ src/mpl_modernqt/backend.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+from matplotlib.backends.backend_qt import _BackendQT, NavigationToolbar2QT, FigureManagerQT
+from matplotlib.backends.backend_qtagg import *
+from matplotlib.backends.qt_editor import _formlayout
+from matplotlib.backends.qt_compat import QtWidgets
+from matplotlib.backends.qt_editor._formlayout import FormDialog
+import qtawesome as qta
+import qdarktheme  # Always import after Qt
+
+
+class FigureCanvasQTAgg2(FigureCanvasQTAgg):
+    def __init__(self, figure=None, *args, **kwargs):
+        super().__init__(figure, *args, **kwargs)
+        qdarktheme.setup_theme('light')
+        # self.figure.set_facecolor('#ebebeb')
+        self.figure.set_facecolor('#f8f9fa')
+
+
+@_BackendQT.export
+class _BackendQTAgg(_BackendQT):
+    FigureCanvas = FigureCanvasQTAgg2
+
+def fedit2(data, title="", comment="", icon=None, parent=None, apply=None):
+    if QtWidgets.QApplication.startingUp():
+        _app = QtWidgets.QApplication([])
+    dialog = FormDialog(data, title, comment, icon, parent, apply)
+    dialog.setStyleSheet("QDialog{background-color: #f8f9fa;}")  # light
+    # dialog.setStyleSheet("QDialog{background-color: #202124;}")  # dark
+    dialog.setWindowIcon(qta.icon('ph.chart-line'))
+
+    if parent is not None:
+        if hasattr(parent, "_fedit_dialog"):
+            parent._fedit_dialog.close()
+        parent._fedit_dialog = dialog
+
+    dialog.show()
+
+
+_formlayout.fedit = fedit2
+
+
+class NV(NavigationToolbar2QT):
+    def __init__(self, canvas, parent=None, coordinates=True):
+        super().__init__(canvas, parent, coordinates)
+        self.setStyleSheet("background : #f8f9fa;")  # light
+        # self.setStyleSheet("background : #202124;")  # dark
+
+        btn_reset_zoom = self.actions()[0]
+        btn_reset_zoom.setIcon(qta.icon('ph.house'))
+
+        btn_arrow_left = self.actions()[1]
+        btn_arrow_left.setIcon(qta.icon('ph.arrow-left'))
+
+        btn_arrow_right = self.actions()[2]
+        btn_arrow_right.setIcon(qta.icon('ph.arrow-right'))
+
+        btn_move = self.actions()[4]
+        btn_move.setIcon(qta.icon('ph.arrows-out-cardinal'))
+
+        btn_zoom = self.actions()[5]
+        btn_zoom.setIcon(qta.icon('ph.magnifying-glass-plus'))
+
+        btn_subplots = self.actions()[6]
+        btn_subplots.setIcon(qta.icon('ph.sliders'))
+
+        btn_customize = self.actions()[7]
+        btn_customize.setIcon(qta.icon('ph.chart-line'))
+
+        btn_save = self.actions()[9]
+        btn_save.setIcon(qta.icon('ph.floppy-disk'))
+
+        self.setMovable(False)
+
+
+FigureManagerQT._toolbar2_class = NV
